e531a3583b61a2430a312bdba9892089ea5172e2
Batradz-T <tasoevbatradz@yandex.ru>
2020-12-14 17:43:08 +0300

    Решение задачи 2.2.2 final.2

--- 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @Controller
 public class HelloController {
+	private List<Car> cars = new ArrayList<>();
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
@@ -20,25 +21,16 @@ public class HelloController {
 		messages.add("Hello!");
 		messages.add("I'm Spring MVC application");
 		messages.add("5.2.0 version by sep'19 ");
+		for (int i = 1; i <= 5; i++ ) {
+			cars.add(new Car("fuel" + i, "transmission" + i, "model" + i));
+		}
 		model.addAttribute("messages", messages);
 		return "index";
 	}
 
 	@GetMapping(value = "/cars")
 	public String printCars(ModelMap model, @RequestParam(defaultValue = "5") int count) {
-//		List<String> messages = new ArrayList<>();
-//		messages.add("Hello, Batradz!");
-//		messages.add("I'm Spring MVC application");
-//		messages.add("5.2.0 version by sep'19 ");
-
 		ServiceCar serviceCar = new ServiceCar();
-
-
-		List<Car> cars = new ArrayList<>();
-		for (int i = 1; i <= 5; i++ ) {
-			cars.add(new Car("fuel" + i, "transmission" + i, "model" + i));
-		}
-
 		model.addAttribute("cars", serviceCar.getCars(cars, count));
 		return "cars";
 	}
